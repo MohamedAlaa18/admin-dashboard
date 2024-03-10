3242986f2924b39c2000a3c5ce0fb43df2ffbcc2
Mohamed Alaa <86682775+MohamedAlaa28@users.noreply.github.com>
2024-03-10 11:54:39 +0200

    add logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,12 +23,13 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
-        <meta name="keywords" content="HTML, CSS, JavaScript, React.js, React, Next.js, Next, CSS, Material Ui, PostgreSQL, Frontend, Front-end ,fullstack ,full-stack" />
+        <meta name="description" content="Admin Dashboard is a comprehensive web application designed to provide administrators with intuitive tools and insights for managing and analyzing data efficiently. Built with modern technologies such as React.js, Next.js, and Material-UI, this dashboard offers a seamless user experience with features including dynamic data visualization, user management, and system analytics." />
+        <meta name="keywords" content="HTML, CSS, JavaScript, React.js, React, Next.js, Next, CSS, Material Ui, Frontend, Front-end" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="author" content="Mohamed Alaa" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" type="image/svg+xml" href="/logo.png" />
-        <title>Admin</title>
+        <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
+        <title>Admin Dashboard</title>
       </head>
       <body className={inter.className} suppressHydrationWarning={true}>
         <ColorModeContext.Provider value={colorMode}>
